04ac1f652ae35d21491b98712506db19dbcfe4ce
Konstantin Lekh <kos@Konstantins-MacBook-Pro.local>
2022-05-18 03:40:19 +0200

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('RMF:v1.2');
+(() => {
+  const nodes = document.querySelectorAll('.main .media img');
+  setInterval(() => {
+    nodes.forEach((node) => {
+      node.src = node.src.indexOf('_1.png') >= 0
+        ? node.src.replace('_1.png', '_2.png')
+        : node.src.replace('_2.png', '_1.png');
+    });
+  }, 1100);
+})();
